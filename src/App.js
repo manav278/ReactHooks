c1b037016d082d7ec43e7c0a0c7197bc23b2e1bf
Manav <patelmanav278@gmail.com>
2024-02-02 11:26:36 +0530

    [Added]: Todo Clearing when clicked on Button added

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ function App() {
   //   val="Code with Patel"
   //   console.log(val);
   // }
-  // -------------------------------
+
+  //--------------------------------
 
   const [name, setName] = useState("Manav")
   const nameChange = () => {
@@ -25,13 +26,56 @@ function App() {
     // }
     name === "Manav" ? setName("Manav Jayesh Patel") : setName("Manav")
   }
+
+  //--------------------------------
+
+  var array = [
+    {
+      id: "1",
+      name: "Admin1",
+      College: "DDU"
+    },
+    {
+      id: "2",
+      name: "Admin2",
+      College: "DDU"
+    },
+    {
+      id: "3",
+      name: "Admin3",
+      College: "DDU"
+    },
+    {
+      id: "4",
+      name: "Admin4",
+      College: "DDU"
+    }
+  ]
+
+  const [myArray, setMyArray] = useState(array)
+  const handleClick = () => {
+    setMyArray([])
+  }
+
   return (
     <div className="App">
-      <h1>{name}</h1>
+
+      {/* <h1>{name}</h1>
       <button onClick={nameChange}>Click here for 'App' Component</button>
       <hr></hr>
+      //--------------------------------
       <Hooksa></Hooksa>
-      <hr></hr>
+      <hr></hr> 
+      //--------------------------------
+      */}
+
+      {
+        myArray.map((currElement) => {
+          return <h1 style={{ color: 'white', backgroundColor: 'black', width: '150px', border: '5px solid orange' }} key={currElement.id}>{currElement.name}<hr></hr>{currElement.College}</h1>
+        })
+      }
+      <button onClick={handleClick}>Click Here to Clear All ToDos</button>
+
     </div>
   );
 }
--- src/components/Hooksa.js
+++ src/components/Hooksa.js
@@ -4,6 +4,7 @@ const Hooksa = () => {
     const changeSt = () => {
         name === "Manav" ? setName("DDU IT") : setName("Manav")
     }
+    // const [name, setName] = React.useState('Manav')
     const [name, setName] = useState('Manav')
     return (
         <div>
