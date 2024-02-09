a99f372b75f357a50175aafe87233b5a9dd96d26
Manav <patelmanav278@gmail.com>
2024-02-09 16:14:39 +0530

    [Updated]:BasicForm Refactored

--- src/components/forms/Basicform.js
+++ src/components/forms/Basicform.js
@@ -6,19 +6,23 @@ export default function Basicform() {
     const [allEntry, setAllEntry] = useState([])
     const handleSubmit = (e) => {
         e.preventDefault()
-        setAllEntry([...allEntry, { email: email, password: password }])
-        console.log(allEntry)
+        if (email && password) {
+            setAllEntry([...allEntry, { email: email, password: password }])
+            console.log(allEntry)
+            setEmail("")
+            setPassword("")
+        }
     }
     return (
         <div>
             <h1>Basic form</h1>
             <form action="" onSubmit={handleSubmit}>
                 <div>
-                    <label htmlFor='email'>Email</label>
+                    <label htmlFor='email'>Email : </label>
                     <input type='email' name='email' id='email' value={email} onChange={(e) => { setEmail(e.target.value) }} />
                 </div>
                 <div>
-                    <label htmlFor='password'>Password</label>
+                    <label htmlFor='password'>Password : </label>
                     <input type='password' name='password' id='password' value={password} onChange={(e) => { setPassword(e.target.value) }} />
                 </div>
                 <button type='submit'>Login</button>
@@ -26,10 +30,11 @@ export default function Basicform() {
             <div>
                 {
                     allEntry.map((x) => {
+                        const { email, password } = x
                         return (
-                            <div key={x.email} className="div-class">
-                                <h1>Email Entered : {x.email}</h1>
-                                <h1>Password Entered : {x.password}</h1>
+                            <div key={email} className="div-class">
+                                <h1>Email Entered : {email}</h1>
+                                <h1>Password Entered : {password}</h1>
                             </div>)
                     })
                 }
