6ae828a6877def7062bcece535a5231a4b9493bc
Manav <patelmanav278@gmail.com>
2024-02-09 14:56:18 +0530

    [Updated]: Basicform Updated

--- src/components/forms/Basicform.js
+++ src/components/forms/Basicform.js
@@ -3,20 +3,37 @@ import React, { useState } from 'react'
 export default function Basicform() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [allEntry, setAllEntry] = useState([])
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setAllEntry([...allEntry, { email: email, password: password }])
+        console.log(allEntry)
+    }
     return (
         <div>
             <h1>Basic form</h1>
-            <form action="">
+            <form action="" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor='email'>Email</label>
-                    <input type='email' name='email' id='email' onChange={(e) => { setEmail(e.target.value) }} />
+                    <input type='email' name='email' id='email' value={email} onChange={(e) => { setEmail(e.target.value) }} />
                 </div>
                 <div>
                     <label htmlFor='password'>Password</label>
-                    <input type='password' name='password' id='password' onChange={(e) => { setEmail(e.target.value) }} />
+                    <input type='password' name='password' id='password' value={password} onChange={(e) => { setPassword(e.target.value) }} />
                 </div>
                 <button type='submit'>Login</button>
             </form>
+            <div>
+                {
+                    allEntry.map((x) => {
+                        return (
+                            <div key={x.email} className="div-class">
+                                <h1>Email Entered : {x.email}</h1>
+                                <h1>Password Entered : {x.password}</h1>
+                            </div>)
+                    })
+                }
+            </div>
         </div>
     )
 }
